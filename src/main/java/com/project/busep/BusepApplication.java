f653c8840147469a580e3d76629b8122cb8949cf
Jovana Lakic <jovana.lakic8@gmail.com>
2025-10-07 20:19:19 +0200

    Initial shell for backend project

---
+++ src/main/java/com/project/busep/BusepApplication.java
@@ -0,0 +1,13 @@
+package com.project.busep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/busep/BusepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.busep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
